91e6c590b3286f869441155515bc22aa73537351
3SSI3 <starsm004@gmail.com>
2022-12-02 05:31:39 +0300

    Dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     import sys
     i = len(sys.argv) - 1
 
-    if i ==0:
+    if i == 0:
         print("{} arguments.".format(i))
     elif i == 1:
-        print("{} arguments:".format(i))
+        print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
 
