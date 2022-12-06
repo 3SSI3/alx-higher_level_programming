28afa66310530e16fa952d108cf157acb25e167b
3SSI3 <starsm004@gmail.com>
2022-12-06 03:10:23 +0300

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if (idx < 0) or (idx < len(my_list) - 1):
+    if idx < 0:
         return (my_list)
-    else:
-        my_list[idx] = element
+
+    length = len(my_list)
+
+    if idx > length - 1:
         return (my_list)
+
+    my_list[idx] = element
+
+    return (my_list)
