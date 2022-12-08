45e868b6360595f29f46c2119101bfd0d3a14bf7
3SSI3 <starsm004@gmail.com>
2022-12-08 05:00:09 +0300

    Search & Replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,9 @@
 #!usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)
+    copy = []
+    for i in range(len(my_list)):
+        if my_list[i] ==  search:
+            copy.append(replace)
+        else:
+            copy.append(my_list[i])
+    return copy
