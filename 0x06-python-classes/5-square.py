e9f9d5272dbf2dfbd66c6242f7b50f6c3c0e1a9b
3SSI3 <starsm004@gmail.com>
2022-12-21 05:14:04 +0300

    Printing a Square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""A class that defines a Square"""
+
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size=0):
+        """Square class initialized"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets the data"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the data"""
+        if (isinstance(value, int) is False):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Return the area"""
+        return self.__size**2
+
+    def my_print(self):
+        """Prints to stdout the square"""
+        if (self.__size == 0):
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print('#', end='')
+            print()
