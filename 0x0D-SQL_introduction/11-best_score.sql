9bb5654cade979185cee9baf9e9908aa83be8238
3SSI3 <starsm004@gmail.com>
2023-02-15 05:38:46 +0300

    Fixes :hammer:

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- lists all records with score >= 10
 
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY Sscore DESC, name;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC, name;
