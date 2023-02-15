079f6a93e444bc9c0d81fda40d253a9f5934d0b8
3SSI3 <starsm004@gmail.com>
2023-02-16 01:41:42 +0300

    Complete :heavy_check_mark:

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,10 +3,10 @@
 -- has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
 
 CREATE DATABASE
-  IF NOT EXISTS 'hbtn_0d_2';
-CREATE USER 
-   IF NOT EXISTS 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
    ON `hbtn_0d_2`.*
    TO 'user_0d_2'@'localhost';
