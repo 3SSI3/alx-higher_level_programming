7a37bb878d6aef516c6c1b62797f9e10e9a049f3
3SSI3 <starsm004@gmail.com>
2023-02-16 01:16:42 +0300

    Fix :hammer:

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,8 @@
+-- create user_0d_1 with all privileges.
+
 CREATE USER
-IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost'
+  ON *.*
+  TO 'user_0d_1'@'localhost'
