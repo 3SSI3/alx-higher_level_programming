e546a81e1f3266249d655b5146abdc3e1e04258f
3SSI3 <starsm004@gmail.com>
2023-02-16 02:18:18 +0300

    Complete :heavy_check_mark:

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,6 +4,6 @@
 
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
-        LEFT JOIN `tv_shows_genres` AS g
-        ON s.`id` = g.`show_id`
-ORDER BY s.`title`, g.`genre_id`;
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
