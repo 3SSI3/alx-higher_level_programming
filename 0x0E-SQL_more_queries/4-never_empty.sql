64dfa4e257c2aef7a0410537a93a34f1da40185d
3SSI3 <starsm004@gmail.com>
2023-02-16 01:37:53 +0300

    Complete :heavy_check_mark:

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- Creates table id_not_null
+--- IF table already exists, script shouldnt fail
+
+CREATE TABLE IF NOT EXISTS `id_not_null` (
+  `id`  INT         DEFAULT 1,
+  `name` VARCHAR(256)
+);
