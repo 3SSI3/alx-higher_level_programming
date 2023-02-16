d7b098256b913e9b87199dc6aa13971d74572751
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-02-16 23:06:29 +0300

    Fix :hammer:

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- Lists all records of the second_table
 -- displays both the score from top first
 
-SELECT score, naem FROM second_table ORDER BY score DESC, name;
+SELECT score, name FROM second_table ORDER BY score DESC, name;
