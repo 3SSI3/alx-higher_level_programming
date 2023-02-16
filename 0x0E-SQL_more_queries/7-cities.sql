890d48410d1b32b55b24cade13f28d2feed10de2
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-02-17 00:01:47 +0300

    Fixed :hammer_and_wrench:

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Creates database and table cities
 -- id INT unique, auto generated, can’t be null and is a primary key
---state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
---name VARCHAR(256) can’t be null.
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) can’t be null.
 
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
