b7304e29cbf0acbb075bf61a8ad7bb2089db0031
3SSI3 <starsm004@gmail.com>
2023-03-15 01:34:21 +0300

    Done :chess_pawn:

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+class Rectangle {
+  counstructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.length = h;
+    }
+  }
+
+  print () {
+    for (let u = 0; u < this.height; u++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
