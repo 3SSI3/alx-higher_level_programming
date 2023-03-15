a302315f4371074d5eb564df96382edd58adef69
3SSI3 <starsm004@gmail.com>
2023-03-15 03:34:48 +0300

    Done

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement)
-let occs = 0;
-for (let i = 0; i < list.length; i++) {
-	if (list[i] === searchElement) {
-		occs;
-	}
-}
-
-return occs;
+exports.nbOccurences = function (list, searchElement) {
+  let nOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
+    }
+  }
+  return nOccurrences;
 };
