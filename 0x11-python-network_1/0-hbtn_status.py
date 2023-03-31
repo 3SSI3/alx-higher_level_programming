69807ed6c353108c432cccf8b7b24dba5b3b883c
3SSI3 <starsm004@gmail.com>
2023-04-01 02:27:17 +0300

    What is my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 
 import urllib.request
 
-with urllib.request.urlopen(https://alx-intranet.hbtn.io/status) as response:
-    body = response.read()
-    print("Body response:")
-    print("- type: {}".format(type(body)))
-    print("- content: {}".format(body))
-    print("- utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == "__main__":
+    with urllib.request.urlopen(https://alx-intranet.hbtn.io/status) as response:
+        body = response.read()
+        print("Body response:")
+        print("- type: {}".format(type(body)))print("- content: {}".format(body))
+        print("- utf8 content: {}".format(body.decode("utf-8")))
