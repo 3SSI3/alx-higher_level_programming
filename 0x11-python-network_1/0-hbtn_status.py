7c752db125fe9511300161570eeffe784c37f571
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-04-01 02:30:20 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(https://alx-intranet.hbtn.io/status) as response:
         body = response.read()
         print("Body response:")
-        print("- type: {}".format(type(body)))print("- content: {}".format(body))
+        print("- type: {}".format(type(body)))
+        print("- content: {}".format(body))
         print("- utf8 content: {}".format(body.decode("utf-8")))
