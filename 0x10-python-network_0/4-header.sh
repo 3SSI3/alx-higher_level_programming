8f33d08ae589a5d994b3d1271a0a9aab82e087d6
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-03-31 11:12:37 +0300

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# sends GET request to URL
-# A header variable X-School-User-Id must be sent with the value 98
-
+# sends GET request to URL, A header variable X-School-User-Id must be sent with the value 98
 curl -sH "X-School-User-Id: 98" "$1"
