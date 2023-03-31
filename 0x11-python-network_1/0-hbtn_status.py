e7ebd3e9d92851b296a6aefec28eaac7da969e07
3SSI3 <starsm004@gmail.com>
2023-04-01 02:05:45 +0300

    What is my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Script fetches alx URL using the urllib package and with statement.
+
+import urllib.request
+
+with urllib.request.urlopen(https://alx-intranet.hbtn.io/status) as response:
+    body = response.read()
+    print("Body response:")
+    print("- type: {}".format(type(body)))
+    print("- content: {}".format(body))
+    print("- utf8 content: {}".format(body.decode("utf-8")))
