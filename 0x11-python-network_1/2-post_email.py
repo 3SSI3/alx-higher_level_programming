138768f73570f18f5a628fb8e657562fb0b76d75
3SSI3 <starsm004@gmail.com>
2023-04-03 01:44:27 +0300

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import sys
 import urllib.parse
 import urllib.request
 
-if __name__== "__main__":
+if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
