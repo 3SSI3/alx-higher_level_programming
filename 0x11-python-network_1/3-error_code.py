e4ff8727bc3121ceaae078705d44ed405aff628d
3SSI3 <starsm004@gmail.com>
2023-04-06 01:58:48 +0300

    Fixed :gear:

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,12 +5,12 @@ the X-Request-Id VAR found in header
 
 from urllib import request
 from urllib import error
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
+        with request.urlopen(sys.argv[1]) as response:
                 body = response.read()
                 print(body.decode('utf-8'))
-        except error.HTTPError as error:
-                print('Error code: {}'.format(error.code))
+    except error.HTTPError as er:
+        print('Error code: {}'.format(er.code))
