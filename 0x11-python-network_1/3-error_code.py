9761ddae7755a7215f3b5605e25268228baee510
3SSI3 <starsm004@gmail.com>
2023-04-06 01:59:54 +0300

    Fixed :gear:

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == "__main__":
     try:
-        with request.urlopen(sys.argv[1]) as response:
+        with request.urlopen(.argv[1]) as response:
                 body = response.read()
                 print(body.decode('utf-8'))
     except error.HTTPError as er:
