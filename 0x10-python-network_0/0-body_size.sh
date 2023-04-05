8c31faa4ab04fc5a7ca29dd39c6235b837788bf1
3SSI3 <starsm004@gmail.com>
2023-04-05 04:10:10 +0300

    Fixed :gear:

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Send a request to URL with curl, Displays the size of the body of the response
-curl -sL "$1"
+read -p "Enter URL: " url
+response=$(curl -s $url)
+size=$(echo -n $response | wc -c)
+echo "Response body size: $size bytes"
