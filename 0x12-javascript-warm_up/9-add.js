22e21225841eb13c33e998593840d017464ed0c4
3SSI3 <starsm004@gmail.com>
2023-04-28 02:54:40 +0300

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
 function add (a, b) {
-  const c = a + b;
-  console.log(c);
+  return (a + b);
 }
-add(Number(process.argv[2], Number(process.argv[3])));
+const args = process.argv.slice(2);
+const a = parseInt(args[0]);
+const b = parseInt(args[1]);
+
+console.log(add(a, b));
