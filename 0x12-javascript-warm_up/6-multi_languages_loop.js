d2d70179e3259432fd699ae49490de531b935e84
3SSI3 <starsm004@gmail.com>
2023-04-27 15:42:56 +0300

    Loop of languages :link:

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
-for (const lines of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-  console.log(lines);
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of lines) {
+  console.log(line);
 }
