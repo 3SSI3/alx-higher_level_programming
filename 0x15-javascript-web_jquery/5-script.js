f79f1dd990a12554bd0bb5fa12a3cdeaa4870c86
3SSI3 <starsm004@gmail.com>
2023-05-04 01:58:27 +0300

    JS QUERY :bricks:

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  $('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
+});
---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  $('DIV#update_header').click(function () {
+  $('HEADER').text('New Header!!!');
+});
